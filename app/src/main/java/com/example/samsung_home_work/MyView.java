e45a27ce0e3aeb9811600d6f19e575cabfaa059c
ania <ania.daa11@gmail.com>
2019-10-19 15:40:11 +0300

    1.7: 1.7.6. Android-практикум: рисование узоров на Canvas : Круг

--- app/src/main/java/com/example/samsung_home_work/MyView.java
+++ app/src/main/java/com/example/samsung_home_work/MyView.java
@@ -17,12 +17,13 @@ public class MyView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         Paint paint = new Paint();
-        int y = 0;
-        while (y < canvas.getHeight()) {
-            canvas.drawLine(0, y,
-                    this.getWidth(), y, paint);
-            y += 30;
-        }
+        paint.setColor(Color.YELLOW);
+        paint.setStyle(Paint.Style.FILL);
+        canvas.drawCircle(300, 300, 200, paint);
+        paint.setColor(Color.RED);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(20);
+        canvas.drawCircle(300, 300, 200, paint);
     }
 }
 
