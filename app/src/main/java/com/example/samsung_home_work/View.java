e27ec93bb4fc80bdebf21f61db9ce1b7925a5434
ania <ania.daa11@gmail.com>
2019-10-19 15:25:42 +0300

    1.7: 1.7.6. Android-практикум: рисование узоров на Canvas : Линии

--- app/src/main/java/com/example/samsung_home_work/View.java
+++ app/src/main/java/com/example/samsung_home_work/View.java
@@ -1,7 +1,9 @@
 package com.example.samsung_home_work;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
+
+/*setContentView(new MyDraw(this));
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,4 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(new MyView(this));
     }
-}
+}*/
