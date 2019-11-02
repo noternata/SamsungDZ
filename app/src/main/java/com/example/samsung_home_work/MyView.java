6478a132ce483a15c669a57af51452e3679ab71a
Daniel <b.d.k@mail.ru>
2019-11-02 13:30:54 +0300

    Много шариков начало

--- app/src/main/java/com/example/samsung_home_work/MyView.java
+++ app/src/main/java/com/example/samsung_home_work/MyView.java
@@ -2,91 +2,39 @@ package com.example.samsung_home_work;
 
 
 import android.content.Context;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.os.CountDownTimer;
+import android.graphics.*;
 import android.view.View;
 
 
 public class MyView extends View {
+    int N = 10; // количество шариков
+    float[] x  = new float[N];
+    float[] y  = new float[N];
+    float[] vx = new float[N];
+    float[] vy = new float[N];
 
-    int N = 15;
-    int[] l = new int [N];
-    double x0, y0;
-    double[] x = new double [N];
-    double[] y = new double [N];
-    double g = 9.832f, pi = Math.PI;
-    double[] w = new double[N];
-    double fi0;
-    double[] fi = new double[N];
-    int t = 0, deltaT = 1;
-
-    void makePendulum()
-    {
-        fi0 = pi/4;
-
-        int l_min = 100;
-        for (int i = 0; i<N; i++)
-        {
-            l[i] = l_min;
-            l_min += 50;
-
-            w[i] = Math.sqrt(g/l[i]);
-        }
-    }
-    void movePendulum()
-    {
-        t += deltaT;
-
-        for (int i = 0; i<N; i++)
-        {
-            fi[i] = fi0 * Math.cos(w[i] * t);
-            x[i] = l[i]*Math.sin(fi[i]);
-            y[i] = l[i]*Math.cos(fi[i]);
-        }
-    }
-
-    MyView(Context context) {
-        super(context);
-        makePendulum();
-        MyTimer timer = new MyTimer();
-        timer.start();
-    }
-
+   MyView(Context context) {
+       super(context);
+       for (int i = 0; i < N; i++){
+           x[i] = (float)(Math.random() * 500);
+           y[i] = (float)(Math.random() * 500);
+           vx[i] = (float)(Math.random() * 6 - 3);
+           vy[i] = (float)(Math.random() * 6 - 3);
+       }
+   }
+    Paint paint = new Paint();
     @Override
     protected void onDraw(Canvas canvas) {
-        x0 = getWidth()/2;
-        y0 = getHeight()/4;
-        Paint paint = new Paint();
-        canvas.drawCircle((float) x0, (float) y0, 10, paint);
-        for (int i = 0; i<N; i++)
-        {
-            paint.setColor(Color.BLUE);
-            canvas.drawLine((float)x0, (float)y0, (float)(x[i] + x0), (float)(y[i]+ y0), paint);
-            paint.setColor(Color.RED);
-            canvas.drawCircle((float)(x[i] + x0), (float)(y[i] + y0), 20, paint);
+        // отрисовываем все шарики
+        for (int i = 0; i < N; i++) {
+            canvas.drawCircle(x[i], y[i], 20, paint);
         }
-    }
-
-    void nextFrame()
-    {
-        movePendulum();
+        // готовим массивы x и у для следущего кадра
+        for (int i = 0; i < N; i++) {
+            x[i] += vx[i];
+            y[i] += vy[i];
+        }
+        //запрашиваем перерисовку
         invalidate();
-    }
-
-    class MyTimer extends CountDownTimer
-    {
-        MyTimer()
-        {
-            super(100000, 100);
-        }
-        @Override
-        public void onTick(long millisUntilFinished) {
-            nextFrame();
-        }
-        @Override
-        public void onFinish() {
-        }
     }
 }
