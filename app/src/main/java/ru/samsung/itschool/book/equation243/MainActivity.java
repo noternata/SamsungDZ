7a3bfd12879de5ac2cd7aaad9eeab358fc89d945
Daniel <b.d.k@mail.ru>
2019-11-09 15:28:02 +0300

    2.4.5 Equation конец
    Если курсор стоит на id, то все работает, по другому выдает ошибку

--- app/src/main/java/ru/samsung/itschool/book/equation243/MainActivity.java
+++ app/src/main/java/ru/samsung/itschool/book/equation243/MainActivity.java
@@ -1,7 +1,6 @@
 package ru.samsung.itschool.book.equation243;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -17,16 +16,15 @@ public class MainActivity extends Activity {
     }
 
     /** Вызывается при нажатии пользователем на кнопку Решить */
-        public void solveEquation(View view) {
-             // ax+b=c
-            double a = Double.parseDouble( ((EditText)
-                    findViewById(R.id.coefficient_a)).getText().toString());
-            double b = Double.parseDouble( ((EditText)
-                    findViewById(R.id.coefficient_b)).getText().toString());
-            double c = Double.parseDouble( ((EditText)
-                    findViewById(R.id.coefficient_c)).getText().toString());
-            TextView result = (TextView) findViewById(R.id.result);
-            result.setText("" + String.valueOf((c - b) / a));
-        }
-
+    public void solveEquation(View view) {
+            //ax+b=c
+        double a = Double.parseDouble( ((EditText)
+                findViewById(R.id.coefficient_a)).getText().toString());
+        double b = Double.parseDouble( ((EditText)
+                findViewById(R.id.coefficient_b)).getText().toString());
+        double c = Double.parseDouble( ((EditText)
+                findViewById(R.id.coefficient_c)).getText().toString());
+        TextView result = (TextView) findViewById(R.id.result);
+        result.setText("" + String.valueOf((c - b) / a));
+    }
 }
